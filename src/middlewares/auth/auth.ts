5ff6af4e3438924ebef9ecf270d1682d4cd4669a
Andrew <andrekravchenko1994@mail.ru>
2023-12-17 21:00:08 +0100

    Add authMiddleware

---
+++ src/middlewares/auth/auth.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express'
+import { HttpStatus } from '../../utils'
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const auth = req.headers['authorization']
+
+  if (!auth) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED)
+    return
+  }
+
+  const [basic, token] = auth.split(' ')
+
+  if (basic !== 'Basic') {
+    res.sendStatus(HttpStatus.UNAUTHORIZED)
+    return
+  }
+
+  const decodedData = Buffer.from(token, 'base64').toString()
+  const [login, password] = decodedData.split(':')
+
+  if (login !== process.env.AUTH_LOGIN || password !== process.env.AUTH_PASSWORD) {
+    res.sendStatus(HttpStatus.UNAUTHORIZED)
+    return
+  }
+
+  next()
+}
